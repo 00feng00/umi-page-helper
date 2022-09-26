6b09b84f1a4c1b11fcde5adaccb87ded981e642c
feng <892751427@qq.com>
2022-09-26 21:54:03 +0800

    feat: service模版修改;

--- src/template/renderService/index.ts
+++ src/template/renderService/index.ts
@@ -1,41 +1,14 @@
 export default 
 `
-import * as API from '../services/api';
+import { request } from 'umi';
 
-export default {
-  namespace:'menu',
+const api = {
+  getMainList: (params: any): Promise<any> =>
+    request(`/api/inquiryOrder/hall`, {
+      method: 'GET',
+      params,
+    }),
+};
 
-  state:{
-    menu:[],
-  },
-
-  effects:{
-  //fetchMenus给到services/apis/menus.js的
-    *fetchMenus({callback},{call ,put}){
-      const response =yield call(API.fetchMenus);
-      yield put({
-      //public是写在下面reducers里,携带的参数是固定的(也就是修改数据)
-        type:'public',
-        payload:{...response, name:'menu'},
-      });
-      if(response.code===0){
-        if(callback) callback(response.data);
-      }
-    },
-  },
-
-  reducers:{
-    public(state, action){
-      return{
-        ...state,
-        [action.payload.name]:action.payload.data,
-      }
-    },
-    clearData(){
-      return{
-        menu:[],
-      }
-    },
-  },
-}
+export default api;
 `;
